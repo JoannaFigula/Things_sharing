ced3b3c88592d6bdcfda0c90e808458e170cec0e
gabryel <pawel.gabryel@coderslab.pl>
2019-08-01 22:25:58 +0200

    feature: change behave in contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
+import classnames from 'classnames';
 
-function validate(name, email, message) {
-    return {
-        email: name.length === 0,
-        password: email.length === 0,
-        message: message.length === 0,
-    };
+const validate = {
+    name: (val) => !/^[^%]{3,}$/.test(val),
+    email: (val) =>
+        !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(val),
+    message: (val) => !/^[^%]{3,}$/.test(val),
 }
 
 
@@ -13,69 +13,49 @@ class Contact extends Component {
     constructor(props){
         super(props);
         this.state = {
-            name: '',
-            email: '',
-            message:'',
-            nameError:false,
-            emailError: false,
-            messageError: false,
-            inFocus: '',
+            name: {
+                label: "Imię",
+                value: "",
+                error: false,
+            },
+            email: {
+                label: "E-mail",
+                value: "",
+                error: false
+            },
+            message:{
+                label: "Message",
+                value: "",
+                error: false
+            },
         }
     }
 
-    handleNameChange = event => {
+    habdleOnChange = name => e => {
         this.setState({
-            name: event.target.value
+            [name]: {...this.state[name], value: e.target.value}
         })
-    };
+    }
 
-    handleEmailChange = event => {
+    handleOnBlur = name => e => {
         this.setState({
-            email: event.target.value
+            [name]: {...this.state[name], error: validate[name](this.state[name].value)}
         })
-    };
-
-    handleMessageChange = event => {
-        this.setState({
-            message: event.target.value
-        })
-    };
+    }
 
     handleFormSubmit = event => {
         event.preventDefault();
+        const data = {}
+        Object.keys(this.state).forEach(el => {
+            data[el] = this.state[el].value
+        })
 
-        const errors = validate(this.state.name, this.state.email, this.state.message);
-
-        const {name, email, message}=this.state;
-        alert("Wypełnij prawidłowo formularz");
-
-        this.setState({
-            nameError: false,
-            emailError: false,
-            messageError: false,
-        });
-
-        if (this.state.name.length <= 2) {
-            this.setState({
-                nameError: 'Imię musi być dłuższe niż 2 znaki'
-            })
-        }
-        if ( this.state.email.length <= 4 ) {
-            this.setState({
-                emailError: 'Email musi być dłuższy niż cztery zanki'
-            })
-        }
-        if (this.state.message.length <= 4) {
-            this.setState({
-                emailError: 'Twoja wiadomość jest za krótka'
-            })
-        }
+        console.log(data)
     }
 
 
     render() {
         const {name, email, message}=this.state;
-        const errors = validate(name, email, message);
 
         return(
             <footer className="footer">
@@ -87,22 +67,46 @@ class Contact extends Component {
                     <form onSubmit={this.handleFormSubmit} className="form--contact">
 
                         <div className="form-group form-50">
-                            <input className={errors.name ? "Imię musi być dłuższe niż 2 znaki" : ""} type="text" name="name"
-                                    value={this.state.name}
-                                    onChange={this.handleNameChange}
-                                    placeholder="Imię"/>{this.state.nameError} </div>
+                            <input
+                                className={classnames({"error": this.state.name.error})}
+                                type="text" name="name"
+                                value={this.state.name.value}
+                                onChange={this.habdleOnChange('name')}
+                                onBlur={this.handleOnBlur('name')}
+                                placeholder={this.state.name.label}/>
+                                {this.state.name.error && 'Imię musi być dłuższe niż 2 znaki'}
+                        </div>
 
                         <div className="form-group form-50">
-                            <input className={errors.email ? "Email musi być dłuższy niż cztery zanki" : ""} type="text" name="email"
-                                   value={this.state.email}
-                                   onChange={this.handleEmailChange}
-                                   placeholder="Email"/>{this.state.emailError} </div>
+                            <input
+                                className={classnames({"error": this.state.email.error})}
+                                type="text" name="name"
+                                value={this.state.email.value}
+                                onChange={this.habdleOnChange('email')}
+                                onBlur={this.handleOnBlur('email')}
+                                placeholder={this.state.email.label}/>
+                            {this.state.email.error && 'Imię musi być dłuższe niż 2 znaki'}
+                        </div>
+
+                        {/*<div className="form-group form-50">*/}
+                            {/*<input className={errors.email ? "Email musi być dłuższy niż cztery zanki" : ""} type="text" name="email"*/}
+                                   {/*value={this.state.email}*/}
+                                   {/*onChange={this.habdleOnChange('email')}*/}
+                                   {/*placeholder="Email"/>{this.state.emailError} </div>*/}
 
                         <div className="form-group">
-                            <textarea className={errors.name ? "Twoja wiadomość jest za krótka" : ""} name="message"
-                                      value={this.state.message}
-                                      onChange={this.handleMessageChange}
-                                      placeholder="Wiadomość" rows="1"></textarea>{this.state.messageError}</div>
+                            <textarea
+                                rows="1"
+                                className={classnames({"error": this.state.message.error})}
+                                type="text"
+                                name="message"
+                                value={this.state.message.value}
+                                onChange={this.habdleOnChange('message')}
+                                onBlur={this.handleOnBlur('message')}
+                                placeholder={this.state.message.label}
+                            ></textarea>
+                            {this.state.message.error && 'Imię musi być dłuższe niż 2 znaki'}
+                        </div>
 
                         <button className="contactBtn" type="submit">Wyślij</button>
                     </form>
